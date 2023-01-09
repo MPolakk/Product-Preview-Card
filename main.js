478ba73a348b3fdc48ad73f797e668b6a94aea82
Marcin Polak <m_polak98@o2.pl>
2023-01-09 19:16:03 +0100

    Add styles for mobile-375px

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
+
 const App = () => {
    return ( 
       <div className="product">
          <img className="product__img" src="./images/image-product-mobile.jpg" alt='product'></img>
+
          <div className="product__desc">
+
             <label>Perfume</label>
+
             <h1>Gabrielle Essence Eau De Parfum</h1>
+
             <p>A floral, solar, and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
+
          </div>
+
          <div className="product__price">
-            <span className="price__current">$149.99</span> <span className="price__prev">$169.99</span>
+
+            <h1 className="price__current">$149.99</h1>
+
+            <span className="price__prev">$169.99</span>
+
             <button className="price__button">
-               
+               <img src="./images/icon-cart.svg"></img>
                Add to Cart
             </button>
+
          </div>
+
       </div>
     );
 }
@@ -21,4 +34,5 @@ const App = () => {
 
       const container = document.getElementById('root');
       const root = ReactDOM.createRoot(container);
-      root.render(<App/>);
+      root.render(
+      <App/>);
