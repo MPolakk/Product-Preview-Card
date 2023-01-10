040e94b6dec190413d11b248f9e69780b06c3463
Marcin Polak <m_polak98@o2.pl>
2023-01-10 19:48:47 +0100

    Add desktop layout

--- main.js
+++ main.js
@@ -2,31 +2,31 @@
 const App = () => {
    return ( 
       <div className="product">
-         <img className="product__img" src="./images/image-product-mobile.jpg" alt='product'></img>
+         <div className="product__img" ></div>
+         <section className="product__2nd">
+            <div className="product__desc">
 
-         <div className="product__desc">
+               <label>Perfume</label>
 
-            <label>Perfume</label>
+               <h1>Gabrielle Essence Eau De Parfum</h1>
 
-            <h1>Gabrielle Essence Eau De Parfum</h1>
+               <p>A floral, solar, and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
 
-            <p>A floral, solar, and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
+            </div>
 
-         </div>
+            <div className="product__price">
 
-         <div className="product__price">
+               <h1 className="price__current">$149.99</h1>
 
-            <h1 className="price__current">$149.99</h1>
+               <span className="price__prev">$169.99</span>
 
-            <span className="price__prev">$169.99</span>
+               <button className="price__button">
+                  <img src="./images/icon-cart.svg"></img>
+                  Add to Cart
+               </button>
 
-            <button className="price__button">
-               <img src="./images/icon-cart.svg"></img>
-               Add to Cart
-            </button>
-
-         </div>
-
+            </div>
+         </section>
       </div>
     );
 }
